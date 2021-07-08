cdbdfb3aace40565a7b7ac0707f24153bb642446
PatrickZimbrao <52924052+PatrickZimbrao@users.noreply.github.com>
2021-07-08 16:02:24 -0300

    Update boxreact.jsx

--- boxreact.jsx
+++ boxreact.jsx
@@ -1,4 +1,5 @@
-function Box(bannerdiv, textdiv, texttitle, image, elemento){
+function Box(bannerdiv, textdiv, texttitle, image){
+    const elemento = documento(texttitle);
     return (
         <div style={{border: '3px solid #F29D00'}}>
             <div class="d-flex justify-content-between bd-highlight mb-3 align-items-center" style={bannerdiv}>
@@ -17,15 +18,14 @@ function Box(bannerdiv, textdiv, texttitle, image, elemento){
         </div>
     )
 }
-function documento (){
-    if(document.getElementById('saiba-mais')!= null)
+function documento (texttitle){
+    if(texttitle == "SAIBA MAIS")
         return elemento = document.getElementById('saiba-mais').childNodes[0].nodeValue;
-    else if(document.getElementById('pausa-para-pipoca') != null)
+    else if(texttitle == "PAUSA PARA PIPOCA")
         return elemento = document.getElementById('pausa-para-pipoca').childNodes[0].nodeValue;
 }
 
 function Saibamais(props) {
-    const element = documento
     const divbanner = {
         background: '#F29D00',
     };
@@ -36,14 +36,13 @@ function Saibamais(props) {
     const texttitle = 'SAIBA MAIS'
     const image = "https://drive.google.com/uc?id=1HCgHMDRb86oTkS5E9knc7LtaJ_VSfk1o"
     return (
-        Box(divbanner, divtext, texttitle, image, element)
+        Box(divbanner, divtext, texttitle, image)
     );
 }
 
 function PausaParaPipoca(props){
-    const element = documento
     const divbanner = {
-        background: '#F29D00',
+        background: '#0554BF',
     };
     const divtext = {
         padding: '#15px !important',
@@ -52,7 +51,7 @@ function PausaParaPipoca(props){
     const texttitle = 'PAUSA PARA PIPOCA'
     const image = "https://drive.google.com/uc?id=10YqcALeY5qyZkW60LlZHgVZInrbYbrLL"
     return (
-        Box(divbanner, divtext, texttitle, image, element)
+        Box(divbanner, divtext, texttitle, image)
     );
 }
 
